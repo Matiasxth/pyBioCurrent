cae867600ff6ac1dfd7c73a0aa190b217841d5ec
MatiasXth <122752258+Matiasxth@users.noreply.github.com>
2025-05-08 18:31:38 -0400

    utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,7 @@
+def normalizar(datos):
+    return (datos - np.min(datos)) / (np.max(datos) - np.min(datos))
 
+def calcular_fft(datos, fs):
+    freqs = np.fft.rfftfreq(len(datos), d=1/fs)
+    fft_vals = np.abs(np.fft.rfft(datos))
+    return freqs, fft_vals
