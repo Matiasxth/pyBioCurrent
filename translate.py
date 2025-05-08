605fe4ffa37400465f1de5728e6f588d7ac7117a
MatiasXth <122752258+Matiasxth@users.noreply.github.com>
2025-05-08 18:27:40 -0400

    translate.py

--- translate.py
+++ translate.py
@@ -1,1 +1,7 @@
-
+def traducir_binario(datos, umbral=0.01):
+    """
+    Convierte una señal en cadena binaria simple.
+    Permite visualizar patrones repetitivos o eventos como pulsos o espigas eléctricas.
+    """
+    binario = ''.join(['1' if x > umbral else '0' for x in datos])
+    return binario
